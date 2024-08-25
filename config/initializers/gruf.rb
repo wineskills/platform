eb5c23745a07a74da943c6ec5423daed05eea6aa
Pierre FILSTROFF <pierre.filstroff@gmail.com>
2024-08-25 18:22:20 +0200

    Use env variable

--- config/initializers/gruf.rb
+++ config/initializers/gruf.rb
@@ -1,5 +1,5 @@
 Gruf.configure do |c|
-  c.server_binding_url = "0.0.0.0:50052"
+  c.server_binding_url = ENV.fetch("GRPC_BINDING_URL", "0.0.0.0:50051")
   c.interceptors.use(
     Gruf::Interceptors::Instrumentation::RequestLogging::Interceptor,
     formatter: :logstash,
