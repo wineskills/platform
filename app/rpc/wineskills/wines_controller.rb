0f23bb61c4c0fa374be9836a748b2c032970bba4
Pierre FILSTROFF <pierre.filstroff@gmail.com>
2024-08-26 09:11:52 +0200

    Added wines search on gRPC API

--- app/rpc/wineskills/wines_controller.rb
+++ app/rpc/wineskills/wines_controller.rb
@@ -12,25 +12,52 @@ module Wineskills
       wines =
         Wine
           .includes(:elaborate, :region, :winery)
-          .decorate
-          .map do |wine|
-            WineskillsRpc::Wine.new(
-              name: wine.name,
-              kind: wine.kind,
-              body: wine.body,
-              acidity: wine.acidity,
-              alcohol_by_volume: wine.alcohol_by_volume,
-              country: wine.country.iso_short_name,
-              elaborate: wine.elaborate.name,
-              region: wine.region.name,
-              winery: wine.winery.name,
-            )
-          end
+          .map { |wine| serialize_wine(wine) }
 
       WineskillsRpc::GetWinesResponse.new(wines:)
     rescue StandardError => e
       set_debug_info(e.message, e.backtrace[0..4])
       fail!(:internal, :internal, "ERROR: #{e.message}")
+    end
+
+    ##
+    # @return [WineskillsRpc::SearchWinesResponse] The wines response
+    #
+    def search_wines
+      query = request.message.query
+      wines =
+        Wine
+          .search(
+            query,
+            includes: %i[elaborate region winery],
+            order: {
+              _score: :desc,
+            },
+          )
+          .map { |wine| serialize_wine(wine) }
+
+      WineskillsRpc::SearchWinesResponse.new(wines:)
+    rescue StandardError => e
+      set_debug_info(e.message, e.backtrace[0..4])
+      fail!(:internal, :internal, "ERROR: #{e.message}")
+    end
+
+    private
+
+    def serialize_wine(wine)
+      wine.decorate.try do |wine|
+        WineskillsRpc::Wine.new(
+          name: wine.name,
+          kind: wine.kind,
+          body: wine.body,
+          acidity: wine.acidity,
+          alcohol_by_volume: wine.alcohol_by_volume,
+          country: wine.country.iso_short_name,
+          elaborate: wine.elaborate.name,
+          region: wine.region.name,
+          winery: wine.winery.name,
+        )
+      end
     end
   end
 end
